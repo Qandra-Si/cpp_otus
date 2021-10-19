a1bcb078ceeb6ba5369bd1235b3f6f423c28ce3c
Qanda Si <qandra.si@gmail.com>
2021-10-20 02:15:05 +0300

    домашнее задание №1

---
+++ src/homework01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <cpp_otus_config.h>
+
+int main(int argc, char* argv[])
+{
+  std::cout
+    << "build " << PROJECT_VERSION << std::endl
+    << "Hello, World!" << std::endl;
+  return EXIT_SUCCESS;
+}
