4102aa0f3fc88122b99f8329810c55cf79898cd1
Alexander S. Basargin <alexander.bsrgin@yandex.ru>
2021-10-22 12:50:20 +0300

    переключился на C++20

--- src/homework01a/ip_filter.cpp
+++ src/homework01a/ip_filter.cpp
@@ -1,4 +1,4 @@
-// -*- mode: c++; coding: utf-8 -*-
+﻿// -*- mode: c++; coding: utf-8 -*-
 
 // От работы со строковым представлением IPv4 адреса было решено отказаться сразу,
 // в противовес с работой с числами. Строковое представление адреса парсится только
@@ -17,6 +17,8 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <bit>
+#include <ranges>
 
 
 std::vector<std::string> split(const std::string & s, char delimiter)
@@ -60,10 +62,10 @@ static int big_endian_cmp(const void *x, const void *y)
   return 0;
 }
 
-#define ntoh32(n32) ((n32>>24)&0xff) |    \
-                    ((n32<<8)&0xff0000) | \
-                    ((n32>>8)&0xff00) |   \
-                    ((n32<<24)&0xff000000)
+#define be32(n32) ((n32>>24)&0xff) |    \
+                  ((n32<<8)&0xff0000) | \
+                  ((n32>>8)&0xff00) |   \
+                  ((n32<<24)&0xff000000)
 
 static int little_endian_cmp(const void *x, const void *y)
 {
@@ -73,11 +75,31 @@ static int little_endian_cmp(const void *x, const void *y)
   // в принципе, продетектировав платформу, можно было бы сразу октеты грузить в структуру ipv4_t
   // в обратном порядке, это бы значительно снизило расходы во время сортировки, он добавило бы
   // путаницы при выводе октет адреса в строковом представлении... не будем усложнять, не сервер пишем
-  _x = ntoh32(_x);
-  _y = ntoh32(_y);
+  _x = be32(_x);
+  _y = be32(_y);
   if (_x < _y) return 1;
   if (_x > _y) return -1;
   return 0;
+}
+
+void print_ipv4(std::ostream& output, const ipv4_t & ipv4)
+{
+  output
+    << (int)ipv4.v_addr[0] << '.'
+    << (int)ipv4.v_addr[1] << '.'
+    << (int)ipv4.v_addr[2] << '.'
+    << (int)ipv4.v_addr[3] << std::endl;
+}
+
+template <typename Pred>
+void filter_ip_pool(std::ostream& output, const std::vector<ipv4_t> & ip_pool, Pred pred)
+{
+  // C++20 поддерживает работу с предикатами фильтрации в ranges,
+  // пользуемся pipe для передачи элементов вектора в фильтрацию
+  for (const auto & ipv4 : ip_pool | std::views::filter(pred))
+  {
+    print_ipv4(output, ipv4);
+  }
 }
 
 int main(int, char* [])
@@ -99,48 +121,46 @@ int main(int, char* [])
       if (line_lexems.empty()) break; // разве этот ввод так может закончиться?
       if (!split_ipv4(line_lexems.front(), ipv4))
       {
-	// работа с неправильными ipv4 адресами не определена условием задачи
-	// впрочем, сделать вид, что всё в порядке тоже не будем
-	throw std::invalid_argument("Illegal IPv4 address format: " + line_lexems.front());
+        // работа с неправильными ipv4 адресами не определена условием задачи
+        // впрочем, сделать вид, что всё в порядке тоже не будем
+        throw std::invalid_argument("Illegal IPv4 address format: " + line_lexems.front());
       }
       ip_pool.push_back(ipv4);
     }
 
     // определяем порядок байт в числе (little-endian или big-endian?)
     // C++20 уже поддерживает нативное определение порядка байт в числе
-    ipv4_t endian;
-    endian.numeric = 0x00000001;
-    
     std::qsort(
       ip_pool.data(), ip_pool.size(), sizeof(ipv4_t),
       // здесь нельзя 'напрямую' сравнивать uint32_t представление ipv4, т.к. на платформах
       // с big-endian представлением чисел, данные отсортируются в обратном порядке
-      endian.v_addr[0] ? little_endian_cmp : big_endian_cmp
+      (std::endian::native == std::endian::little) ? little_endian_cmp : big_endian_cmp
     );
 
-    // выводим адреса из пула в отсортированном виде
-    for (const auto & ipv4 : ip_pool)
-    {
-      std::cout << (int)ipv4.v_addr[0] << '.' << (int)ipv4.v_addr[1] << '.' << (int)ipv4.v_addr[2] << '.' << (int)ipv4.v_addr[3] << std::endl;
-    }
+    // выводим адреса из пула в отсортированном виде (без фильтрации)
+    filter_ip_pool(
+        std::cout,
+        ip_pool,
+        [](const ipv4_t&) { return true; }
+    );
     // выводим адреса из пула, первый октет которых равен 1
-    for (const auto & ipv4 : ip_pool)
-    {
-      if (ipv4.v_addr[0] != 1) continue;
-      std::cout << (int)ipv4.v_addr[0] << '.' << (int)ipv4.v_addr[1] << '.' << (int)ipv4.v_addr[2] << '.' << (int)ipv4.v_addr[3] << std::endl;
-    }
+    filter_ip_pool(
+        std::cout,
+        ip_pool,
+        [](const ipv4_t& ipv4) { return ipv4.v_addr[0] == 1; }
+    );
     // выводим адреса из пула, первый октет которых равен 46, а второй 70
-    for (const auto & ipv4 : ip_pool)
-    {
-      if ((ipv4.v_addr[0] != 46) || (ipv4.v_addr[1] != 70)) continue;
-      std::cout << (int)ipv4.v_addr[0] << '.' << (int)ipv4.v_addr[1] << '.' << (int)ipv4.v_addr[2] << '.' << (int)ipv4.v_addr[3] << std::endl;
-    }
-    // выводим адреса из пула, первый октет которых равен 46, а второй 70
-    for (const auto & ipv4 : ip_pool)
-    {
-      if ((ipv4.v_addr[0] != 46) && (ipv4.v_addr[1] != 46) && (ipv4.v_addr[2] != 46) && (ipv4.v_addr[3] != 46)) continue;
-      std::cout << (int)ipv4.v_addr[0] << '.' << (int)ipv4.v_addr[1] << '.' << (int)ipv4.v_addr[2] << '.' << (int)ipv4.v_addr[3] << std::endl;
-    }
+    filter_ip_pool(
+        std::cout,
+        ip_pool,
+        [](const ipv4_t& ipv4) { return (ipv4.v_addr[0] == 46) && (ipv4.v_addr[1] == 70); }
+    );
+    // выводим адреса из пула, любой октет которых равен 46
+    filter_ip_pool(
+        std::cout,
+        ip_pool,
+        [](const ipv4_t& ipv4) { return (ipv4.v_addr[0] == 46) || (ipv4.v_addr[1] == 46) || (ipv4.v_addr[2] == 46) || (ipv4.v_addr[3] == 46); }
+    );
   }
   catch (const std::exception &e)
   {
