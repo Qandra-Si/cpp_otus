62c3d1a6ab560a8d443aaa54ecc1322c417e31f2
Qandra Si <qandra.si@gmail.com>
2022-03-22 15:58:40 +0300

    незаконченное ДЗ №21

---
+++ src/homework21_boost/initializer.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+
+#include <cpp_otus_config.h>
+#include <cpp_otus_version.h>
+#include "initializer.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+
+namespace homework21 {
+
+std::istream& operator>> (std::istream& in, algorithm_t& hash)
+{
+  std::string token;
+  in >> token;
+  boost::to_lower(token);
+
+  if (token == "crc32")
+  {
+    hash = algorithm_t::crc32;
+  }
+  else if (token == "md5")
+  {
+    hash = algorithm_t::md5;
+  }
+  else
+  {
+    throw po::validation_error(po::validation_error::kind_t::invalid_option_value, "invalid hash algorithm");
+  }
+
+  return in;
+}
+
+bool init_startup_params(int ac, char **av, startup_params_t &params)
+{
+  po::options_description generic_options("Опции общего назначения"); // параметры, разрешённые для ввода в командной строке
+  generic_options.add_options()
+    ("version,v", "выводит строку с версией")
+    ("help,h", "отображает эту справочную информацию")
+    ("path,p", po::value<std::vector<std::string>>()->multitoken()->required(), "директории для сканирования")
+    ("exclude,e", po::value<std::vector<std::string>>()->multitoken(), "директории для исключения из сканирования")
+    ("level,l", po::value<unsigned>(), "директории для сканирования")
+    ("min_size,s", po::value<unsigned long long>()->default_value(1ull), "минимальный размер файла, октет (больше 0)")
+    ("template,t", po::value<std::vector<std::string>>()->multitoken(), "маски имен файлов разрешенных для сравнения (регистронезависимы)")
+    ("block,b", po::value<unsigned>(), "размер блока, которым производятся чтения файлов")
+    ("algorithm,a", po::value<algorithm_t>(), "алгоритм хэширования:\n 0) crc32\n 1) md5")
+    ;
+  po::options_description hidden_options("Скрытые опции"); // скрытые от пользователя параметры, но разрешённые для ввода в командной строке, и из конфигурационного файла
+  hidden_options.add_options()
+    ("verbose", "включение режима отладки")
+    ;
+
+  po::options_description cmdline_options; // параметры командной строки
+  po::options_description visible_options; // видимые пользователю параметры
+  cmdline_options.add(generic_options).add(hidden_options);
+  visible_options.add(generic_options);
+
+  bool parse_error_detected = false;
+  po::variables_map vm;
+  try
+  {
+    po::store(po::parse_command_line(ac, av, cmdline_options/*, po::command_line_style::unix_style, check_multistream_nonconventional_names*/), vm);
+    po::notify(vm); // проверяем диапазоны входных значений
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "ОШИБКА! " << e.what() << std::endl << "См. справочную информацию с ключём help." << std::endl;
+    parse_error_detected = true;
+  }
+
+  if (parse_error_detected || vm.count("help"))
+  {
+    std::cout << "bayan " PROJECT_VERSION << std::endl << std::endl;
+    visible_options.print(std::cout);
+    return false;
+  }
+  if (vm.count("version"))
+  {
+    std::cout << "bayan " PROJECT_VERSION << std::endl;
+    return false;
+  }
+
+  // инициализация параметров приложения
+  params.path = vm["path"].as<std::vector<std::string>>();
+  if (vm.count("exclude"))
+  {
+    const auto& v = vm["exclude"].as<std::vector<std::string>>();
+    params.exclude_path.reserve(v.size());
+    for (const auto& e : v)
+      params.exclude_path.push_back(boost::filesystem::path(e).remove_trailing_separator().string());
+  }
+  if (!vm["level"].empty())
+    params.level = vm["level"].as<unsigned>();
+  params.min_file_size = vm["min_size"].as<unsigned long long>();
+  if (vm.count("template"))
+    params.filename_masks = vm["template"].as<std::vector<std::string>>();
+  if (vm.count("block"))
+    params.block_size = vm["block"].as<unsigned>();
+  if (vm.count("algorithm"))
+    params.algorithm = vm["algorithm"].as<algorithm_t>();
+  params.verbose = vm.count("verbose") > 0;
+
+  return true;
+}
+
+}
---
+++ src/homework21_boost/initializer.h
@@ -0,0 +1,52 @@
+﻿// -*- mode: c++; coding: utf-8 -*-
+/*! \file initializer.h
+* \brief Настройки запуска домашнего задания 21ой лекции
+*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <boost/optional.hpp>
+
+
+/*! \defgroup homework21 Интерфейсные классы программы поиска файлов на диске
+*
+* Подробное описание назначения классов см. в корневом README.md файле.
+*/
+
+/*! \addtogroup homework21
+* @{
+*/
+
+namespace homework21 {
+
+/*! \brief Поддерживаемые алгоритмы хеширования
+*/
+enum class algorithm_t
+{
+  crc32,
+  md5,
+};
+
+/*! \brief Настройки программы
+*/
+struct startup_params_t
+{
+  std::vector<std::string> path; /*!< директории для сканирования */
+  std::vector<std::string> exclude_path; /*!< директории для исключения из сканирования */
+  boost::optional<unsigned> level; /*!< уровень сканирования (один на все директории, 0 - только указанная директория без вложенных) */
+  unsigned long long min_file_size; /*!< минимальный размер файла, октет */
+  std::vector<std::string> filename_masks; /*!< маски имен файлов разрешенных для сравнения (не зависят от регистра) */
+  boost::optional<unsigned> block_size{ 8192 }; /*!< размер блока, которым производятся чтения файлов */
+  algorithm_t algorithm{ algorithm_t::crc32 }; /*!< алгоритм хэширования (crc32, md5) */
+
+  bool verbose; /*!< работа в отладочном (verbose) режиме */
+};
+
+bool init_startup_params(int ac, char** av, startup_params_t& params);
+
+}
+
+/*! @} */
